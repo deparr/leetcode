7888b6f91b5fd01d9ae96d0463dee1a696524b26
David Parrott <dmparr22@byu.edu>
2022-10-31 22:05:32 -0600

    finish lru cache

---
+++ lru-cache/main-test.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"fmt"
+)
+
+type LRUCache struct {
+    head *CacheNode
+    tail *CacheNode
+    
+    pointers map[int]*CacheNode
+    
+    capacity int
+    size int
+}
+
+type CacheNode struct {
+    Key int
+    Val int
+    Next *CacheNode
+    Prev *CacheNode
+}
+
+
+func Constructor(capacity int) LRUCache {
+    nhead := CacheNode{Key: -1, Val: -1, Next: nil, Prev: nil}
+    return LRUCache{head: &nhead, tail: &nhead, pointers: map[int]*CacheNode{}, capacity: capacity, size: 0}
+}
+
+
+func (this *LRUCache) Get(key int) int {
+
+    node, pres := this.pointers[key]
+    if !pres {
+        return -1
+    }
+    
+    if node != this.head {
+
+        if node == this.tail {
+            this.tail = node.Prev
+        }
+
+        if node.Next != nil {
+            node.Next.Prev = node.Prev
+        }
+        node.Prev.Next = node.Next
+        
+
+
+        node.Next = this.head
+        this.head.Prev = node
+        node.Prev = nil
+        this.head = node   
+    }
+
+
+    return node.Val
+}
+
+
+func (this *LRUCache) Put(key int, value int)  {
+    if this.size == 0 {
+        this.head.Key = key
+        this.head.Val = value
+        this.size++
+        this.pointers[key] = this.head
+        return
+    }
+
+    node, pres := this.pointers[key]
+    if pres {
+        node.Val = value
+        if node == this.head {
+            return
+        }
+
+        node.Prev.Next = node.Next
+        if node == this.tail {
+            this.tail = this.tail.Prev
+        } else {
+            node.Next.Prev = node.Prev
+        }
+        node.Prev = nil
+        this.head.Prev = node
+        node.Next = this.head
+        this.head = node
+        return
+    }
+
+    new_node := &CacheNode{Key: key, Val: value, Next: this.head, Prev: nil}
+    this.head.Prev = new_node
+    this.head = new_node
+
+    this.size++
+    if this.size > this.capacity  {
+        this.size--
+
+        delete(this.pointers, this.tail.Key)
+        this.tail = this.tail.Prev
+        this.tail.Next = nil
+    }
+    this.pointers[key] = new_node
+}
+
+
+func main() {
+	lru  := Constructor(2)
+    vals := make([]int, 1)
+
+    lru.Put(2, 1)
+	fmt.Println(*lru.head, *lru.tail, lru.pointers)
+    vals = append(vals, 0)
+    lru.Put(1, 1)
+	fmt.Println(*lru.head, *lru.tail, lru.pointers)
+    vals = append(vals, 0)
+    lru.Put(2, 3)
+	fmt.Println(*lru.head, *lru.tail, lru.pointers)
+    vals = append(vals, 0)
+    lru.Put(4, 1)
+	fmt.Println(*lru.head, *lru.tail, lru.pointers)
+	vals = append(vals, 0)
+    vals = append(vals, lru.Get(1))
+	fmt.Println(*lru.head, *lru.tail, lru.pointers)
+    vals = append(vals, lru.Get(2))
+	fmt.Println(*lru.head, *lru.tail, lru.pointers)
+    fmt.Println(vals)
+}
