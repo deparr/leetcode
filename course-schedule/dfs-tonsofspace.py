bece9a0a1b4191d8ce32293dc2ef8e49493be98a
David Parrott <dmparr22@byu.edu>
2022-11-10 12:35:12 -0700

    n207

---
+++ course-schedule/dfs-tonsofspace.py
@@ -0,0 +1,49 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        prepost = [[-1, -1, i] for i in range(numCourses)]
+
+        deplist = {}
+        for p in prerequisites:
+            if p[1] not in deplist:
+                deplist[p[1]] = {p[0]}
+            else:
+                deplist[p[1]].add(p[0])
+
+        currorder = 1
+        cycle = False
+        def explore(node, target):
+            nonlocal currorder
+            nonlocal cycle
+            
+            vis.add(node)
+            if node in currpathvis:
+                cycle = True
+                return
+
+            prepost[node][0] = currorder
+            currorder += 1
+            if node in deplist:
+                currpathvis.add(node)
+                for dep in deplist[node]:
+                    if dep not in vis:
+                        explore(dep, node)
+                    elif prepost[dep][1] == -1:
+                        cycle = True
+                        return
+                    if cycle:
+                        return
+
+            prepost[node][1] = currorder
+            currorder += 1
+
+
+        vis = set()
+        currpathvis = set()
+        for i in range(numCourses):
+            if i not in vis:
+                explore(i, i)
+            currpathvis.clear()
+            if cycle:
+                return False
+
+        return True
