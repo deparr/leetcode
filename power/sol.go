6c5a2ef7d15f4cf52d8ee4754a7ba2958b09c235
parromd <me@davidparrott.dev>
2023-01-11 18:48:55 -0700

    clean up

--- power/sol.go
+++ power/sol.go
@@ -1,11 +1,9 @@
 func myPow(x float64, n int) float64 {
     var helper func (x float64, n int) float64
-    var fixed_x float64
+    var fixed_x float64 = x
 
     if n < 0 {
         fixed_x = 1 / x
-    } else {
-      fixed_x = x
     }
 
     helper = func (x float64, n int) float64 {
